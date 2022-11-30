bff442d377ea6ff25586bcdc1c86089ef4ab0f4f
Lucas Bortolazzo <lucas.dolberth@yahoo.com>
2022-11-30 00:48:16 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fiscon';
+export class AppComponent implements OnInit, AfterViewInit {
+
+  @ViewChild(MatSort) private sort!: MatSort;
+
+  public pessoaForm!: FormGroup;
+  public displayedColumns = ['id', 'nome', 'telefone', 'excluir'];
+  dataSource = new MatTableDataSource([] as any);
+
+  constructor(private fb: FormBuilder,
+    private snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.buildFormPessoa();
+  }
+
+  ngAfterViewInit() {
+    this.configurarView();
+  }
+
+  private buildFormPessoa() {
+    this.pessoaForm = this.fb.group({
+      id: [{ value: null, disabled: true }, Validators.required],
+      nome: [null, Validators.required],
+      telefone: [null, Validators.required]
+    });
+  }
+
+  private configurarView() {
+    this.dataSource.sort = this.sort;
+  }
+
+  public gravarPessoa() {
+    const novaPessoa = {
+      id: this.dataSource.data.length + 1,
+      nome: this.pessoaForm.get('nome')?.value,
+      telefone: this.pessoaForm.get('telefone')?.value
+    };
+
+    this.dataSource.data.push(novaPessoa);
+
+    this.dataSource.data = [...this.dataSource.data];
+    this.exibirMensagemInformacao('Registro gravado com sucesso!');
+    this.pessoaForm.reset();
+  }
+
+  public excluirPessoa(rowIndex: number) {
+    this.dataSource.data.splice(rowIndex, 1);
+    this.dataSource.data = [...this.dataSource.data];
+    this.exibirMensagemInformacao('Registro excluído com sucesso!');
+  }
+
+  private exibirMensagemInformacao(mensagem: string, duracao?: number,) {
+    this.snackBar.open(mensagem, 'OK', {
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+      duration: duracao ? duracao : 5000,
+    });
+  }
+
+  public applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +17,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSnackBarModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
